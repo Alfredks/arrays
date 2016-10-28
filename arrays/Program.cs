e1f42d79f49aec640c27362842b3b0f1eee1bfbb
alfredks <alfredks@WS-VGY-1941>
2016-10-28 15:16:04 +0200

    dwa

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -10,6 +10,49 @@ namespace arrays
     {
         static void Main(string[] args)
         {
+            //övning 1 
+            /*   string[] namn = new string[10];
+               for (int i = 0; i <10; i++)
+               {
+                   Console.WriteLine("mata in ett unikt namn ");
+                   namn[i] = Console.ReadLine();
+
+               }
+               for (int x = 0; x <10; x++)
+
+               {
+                   Console.WriteLine(namn[x]);
+               }
+               //övning2
+            int[] numbers = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("mata in ett heltal");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            for (int x = 9; x >= 0; x--)
+            {
+                Console.WriteLine(numbers[x]);
+            }
+            
+            int[] numbarray = { 12, 5, 6, 1, 2, 3, -2, -7, 0 };
+            int z = 0;
+            for (int i = 0; i < numbarray.Length; i++)
+            {
+                for (int x = i + 1; x < numbarray.Length; x++)
+                {
+                    if (numbarray[i] > numbarray[x])
+                    {
+                        z = numbarray[x];
+                        numbarray[x] = numbarray[i];
+                        numbarray[i] = z;
+
+                    }
+                }
+                Console.WriteLine(numbarray[i]);
+            }
+            Console.ReadLine();
+        } */
+        //övning
         }
     }
-}
